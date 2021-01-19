7cfd55b6a45611ec81647f440ef2036ba5811894
Tuan.Hoang2 <61672745+Sotatek-TuanHoang2@users.noreply.github.com>
2021-01-19 11:29:33 +0700

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-path='/home/sotatek/Music/cd/hi'
+path='/var/www/api/storage/logs'
 size=$(du -k $path | awk '{ print $1 }')
 echo "size file=$size kilobytes"
 minimumsize=5242880
-minimumsizefile=0
-if ((SIZE>minimumsize)) ; then
+minimumsizefile=1048576
+if [[ $size -gt $minimumsize ]]; then
     echo size is over $minimumsize kilobytes
-    for file in $(ls -lt $path | awk '{ print $10 }'); do
+    for file in $(ls -lt $path | awk '{ print $9 }'); do
         sizefile=$(du -k $path'/'$file | awk '{ print $1 }')
         echo $sizefile
-        if [[ $file == *".log" && $file == "laravel-"* && $sizefile -gt $minimumsizefile ]]; then 
+        if [[ $file == *".log" && $file == "laravel-"* && $sizefile -gt $minimumsizefile ]]; then
             > $path'/'$file
         fi
     done
 else
     echo size is under $minimumsize kilobytes
 fi
-
-
-
