31baed749ea1f4c30c62f65336a18b7e8b352bb3
sotatek-vietha <68310497+sotatek-vietha@users.noreply.github.com>
2021-01-19 10:08:28 +0700

    update file

--- remove.sh
+++ remove.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 path='/home/sotatek/Music/cd/hi'
-size=$(du -k hi | awk '{ print $1 }')
-echo "size=$size kilobytes"
-minimumsize=5242880 
-if ((SIZE>minimumsize)) ; then
+size=$(du -k $path | awk '{ print $1 }')
+echo "size file=$size kilobytes"
+minimumsize=5242880
+minimumsizefile=0
+if ((SIZE<minimumsize)) ; then
     echo size is over $minimumsize kilobytes
     for file in $(ls -lt $path | awk '{ print $10 }'); do
-        if [[ $file == *".log" && $file == "larvel-"* ]]; then
-            : > $path'/'$file
+        sizefile=$(du -k $path'/'$file | awk '{ print $1 }')
+        echo $sizefile
+        if [[ $file == *".log" && $file == "laravel-"* && $sizefile -gt $minimumsizefile ]]; then 
+            > $path'/'$file
         fi
-      done
+    done
 else
     echo size is under $minimumsize kilobytes
 fi
+
+
 
