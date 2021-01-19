5a8178c0fbf0835ab2a8bd549e77042336fdabeb
sotatek-vietha <68310497+sotatek-vietha@users.noreply.github.com>
2021-01-19 10:09:15 +0700

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -4,7 +4,7 @@ size=$(du -k $path | awk '{ print $1 }')
 echo "size file=$size kilobytes"
 minimumsize=5242880
 minimumsizefile=0
-if ((SIZE<minimumsize)) ; then
+if ((SIZE>minimumsize)) ; then
     echo size is over $minimumsize kilobytes
     for file in $(ls -lt $path | awk '{ print $10 }'); do
         sizefile=$(du -k $path'/'$file | awk '{ print $1 }')
